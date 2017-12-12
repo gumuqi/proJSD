a81f3fee613184383e81a16cbce1e61ca0ce4d06
hzfanyang <hzfanyang@corp.netease.com>
2017-12-12 19:58:49 +0800

    footer

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -9,9 +9,19 @@ class Footer extends React.Component{
 			<div id="footer">
 				<div className="container">
 					<div className="info-wrap">
-						<div className="map"><img src={require("../../img/addr.jpg")} /></div>
-						<div className="info"></div>
-						<div className="qrcode"></div>
+						<div className="map">
+							<img src={require("../../img/addr.jpg")} />
+						</div>
+						<div className="info">
+							<h3>联系方式:</h3>
+							<span>联系人：陈学锋先生</span>
+							<span>电话：+86 0514 87757590</span>
+							<span>传真：+86 0514 87757359</span>
+							<span>地址: 中国 江苏 扬州市邗江区 酒甸工业区弘扬西路</span>
+						</div>
+						<div className="qrcode">
+							<img src={require("../../img/qrcode.jpg")} />
+						</div>
 					</div>
 					<div className="right">Copyright ©2017 Jinsd Powered By chuangzhe  technology.</div>
 				</div>
