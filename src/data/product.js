90f7ee22a407a11118223f4900975ceec3702c3b
hzfanyang <hzfanyang@corp.netease.com>
2017-12-14 19:46:20 +0800

    afgadsf

--- src/data/product.js
+++ src/data/product.js
@@ -12,57 +12,57 @@ export default {
 	},{
 		src: './src/prolist/厚转塔标准系列/DSC_6091-厚塔模具A工位打点模（样冲模）.jpg'
 	},{
-		src: '/src/prolist/厚转塔标准系列/DSC_6093-厚塔模具B工位标准系列.jpg'
+		src: './src/prolist/厚转塔标准系列/DSC_6093-厚塔模具B工位标准系列.jpg'
 	},{
-		src: '/src/prolist/厚转塔标准系列/DSC_6099-厚塔模具B工位长方凸苞模.jpg'
+		src: './src/prolist/厚转塔标准系列/DSC_6099-厚塔模具B工位长方凸苞模.jpg'
 	},{
-		src: '/src/prolist/厚转塔标准系列/挤压成型模具.jpg'
+		src: './src/prolist/厚转塔标准系列/挤压成型模具.jpg'
 	},{
-		src: '/src/prolist/厚转塔标准系列/挤压成型模具.jpg'
+		src: './src/prolist/厚转塔标准系列/挤压成型模具.jpg'
 	},{
-		src: '/src/prolist/厚转塔标准系列/挤压成型模具.jpg'
+		src: './src/prolist/厚转塔标准系列/挤压成型模具.jpg'
 	},{
-		src: '/src/prolist/厚转塔标准系列/挤压成型模具.jpg'
+		src: './src/prolist/厚转塔标准系列/挤压成型模具.jpg'
 	},{
-		src: '/src/prolist/厚转塔标准系列/挤压成型模具.jpg'
+		src: './src/prolist/厚转塔标准系列/挤压成型模具.jpg'
 	},{
-		src: '/src/prolist/厚转塔标准系列/挤压成型模具.jpg'
+		src: './src/prolist/厚转塔标准系列/挤压成型模具.jpg'
 	}],
 	"厚转塔经济系列": [],
 	"厚转塔90系列": [{
-		src: '/src/prolist/厚转塔90系列/DSC_6089-厚塔模具A工位90系列.jpg'
+		src: './src/prolist/厚转塔90系列/DSC_6089-厚塔模具A工位90系列.jpg'
 	},{
-		src: '/src/prolist/厚转塔90系列/DSC_6095-厚塔模具B工位90系列.jpg'
+		src: './src/prolist/厚转塔90系列/DSC_6095-厚塔模具B工位90系列.jpg'
 	},{
-		src: '/src/prolist/厚转塔90系列/DSC_6106-厚塔模具C工位90系列.jpg'
+		src: './src/prolist/厚转塔90系列/DSC_6106-厚塔模具C工位90系列.jpg'
 	},{
-		src: '/src/prolist/厚转塔90系列/DSC_6107-厚塔模具D工位90系列.jpg'
+		src: './src/prolist/厚转塔90系列/DSC_6107-厚塔模具D工位90系列.jpg'
 	}],
 	"成型模具": [],
 	"机床附件": [],
 	"村田系列": [{
-		src: '/src/prolist/村田系列/DSC_6122-村田系列模具.jpg'
+		src: './src/prolist/村田系列/DSC_6122-村田系列模具.jpg'
 	}],
 	"自动磨具研磨机": [],
 	"薄转塔系列模具": [{
-		src: '/src/prolist/薄转塔系列模具/DSC_6124-薄转塔系列模具.jpg'
+		src: './src/prolist/薄转塔系列模具/DSC_6124-薄转塔系列模具.jpg'
 	},{
-		src: '/src/prolist/薄转塔系列模具/DSC_6125-薄转塔系列百叶窗模具.jpg'
+		src: './src/prolist/薄转塔系列模具/DSC_6125-薄转塔系列百叶窗模具.jpg'
 	},{
-		src: '/src/prolist/薄转塔系列模具/DSC_6126-薄转塔系列大工位模具.jpg'
+		src: './src/prolist/薄转塔系列模具/DSC_6126-薄转塔系列大工位模具.jpg'
 	},{
-		src: '/src/prolist/薄转塔系列模具/DSC_6127-薄转塔系列小工位模具.jpg'
+		src: './src/prolist/薄转塔系列模具/DSC_6127-薄转塔系列小工位模具.jpg'
 	},],
 	"通快系列": [{
-		src: '/src/prolist/通快系列/DSC_6087-厚塔模具A工位快调系列.jpg'
+		src: './src/prolist/通快系列/DSC_6087-厚塔模具A工位快调系列.jpg'
 	},{
-		src: '/src/prolist/通快系列/DSC_6096-厚塔模具B工位快调系列.jpg'
+		src: './src/prolist/通快系列/DSC_6096-厚塔模具B工位快调系列.jpg'
 	},{
-		src: '/src/prolist/通快系列/DSC_6121-通块系列模具.jpg'
+		src: './src/prolist/通快系列/DSC_6121-通块系列模具.jpg'
 	}],
 	"重载系列模具": [{
-		src: '/src/prolist/重载系列模具/DSC_6086-厚塔模具A工位重载系列.jpg'
+		src: './src/prolist/重载系列模具/DSC_6086-厚塔模具A工位重载系列.jpg'
 	},{
-		src: '/src/prolist/重载系列模具/DSC_6098-厚塔模具B工位重载系列.jpg'
+		src: './src/prolist/重载系列模具/DSC_6098-厚塔模具B工位重载系列.jpg'
 	}]
 }
