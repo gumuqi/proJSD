f04911b5d39c0b6a428ccfbc5e7396faacbc0fd4
hzfanyang <hzfanyang@corp.netease.com>
2017-12-14 20:49:26 +0800

    about us

--- src/data/about.js
+++ src/data/about.js
@@ -4,10 +4,16 @@
 export default {
 	"Chinese": {
 		title: "公司概况",
-		content: "扬州金斯盾模具科技有限公司，主要从事数控转塔冲床模具和机床附件的"
+		content: ["扬州金斯盾模具科技有限公司，主要从事数控转塔冲床模具和机床附件的研发和生产。公司现有精密CNC设备40台，美国哈斯加工中心、日本冈本精密磨床、瑞士阿奇、日本沙迪克等高精度生产设备为产品的质量提供了有力的保障。",
+				  "公司拥有美国福摩斯光测机、海克斯康三坐标测量仪、瑞士丹青测高仪、日本尼康投影机、大型工具显微镜等完善的检测设备。",
+				  "公司开发的数控冲床模具在核心技术上拥有独立的知识产权，能够为AMADA、STRIPPIT、MURATEC、TRUMPF及江苏金方圆、江苏亚威、江苏扬力等国内外数控转塔机床提供专用模具的配套和服务，并可根据客户的特殊要求，制造各类模具和精密机械加工件。",
+				  " 我们永远坚持“持续改进、追求卓越”的质量方针，锐意进取，做大做强民族模具品牌。竭诚为国内广大客户提供优质的产品和满意的"]
 	},
 	"English": {
 		title: "Company profile",
-		content: "xxxxxxxxxxxxxxx"
+		content: ["Yangzhou Kingston Mold Technology Co.Ltd,mainly engaged in CNC turret punch mold and machine tool accessories R&D andproduction. The company's eisting CNC precision eqipment40,Haas processing center,Japan Okamoto precision grinder,Switzerland,Japan, Sadengh and other high precision production equipment to provide a strong guarantee for the quality of the products.",
+				  "The company has the Formos optical machine ,hexagon three coordinate measuring instrument,measuring the Swiss DanGrohe,Japan Nikon projector,sound detection equipment large tool microscope",
+				  "CNC punch die developed with independent intellectual property rights in the core technology,to AMADA ,STRIPPIT,MURATEC,TRUMPF and jiangsu Jin Fangyuan,jiangsu Yawei ,jingsu Yangli CNC turret machine at home and abroad toprovide special mould support and service and according to the special requirements of customers ,manufacturing various types of mold and precision machining ",
+				  "We always adhere to the“continuous improvement,the pursuit of excellence”quality policy,forge ahead,bigger and stronger natoinal mold brand Wholeheartedly for the domestic customers to provide quality products and satisfactory service."]
 	}
 }
--- src/js/components/about/about.js
+++ src/js/components/about/about.js
@@ -9,7 +9,16 @@ class About extends React.Component{
 		return(
 			<div className="about-p">
 				<div className="title">{this.props.title}</div>
-				<p>{this.props.content}</p>
+				{
+					this.props.content.map(function(item, index){
+						if(index<4){
+							return <p key={index}><span style={{width:"30px",height:"22px",display:"inline-block"}}></span>{item}</p>
+						}else{
+							return <p key={index}>......</p>
+						}
+						
+					})
+				}
 			</div>
 		)
 	}
